c5439ecb78f86c1fc99798a4a7e082e3ce228801
Ajinkya <ajinkya.salunke.official@gmail.com>
2024-11-25 10:57:04 +0530

    This is first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!, this is home page')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>Please login here</h1>')
+})
+
+app.get('/register', (req, res)=>{
+    res.send('<h1>Please register here</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
